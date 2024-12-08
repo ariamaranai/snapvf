73bafa96537308067664e63b05b5f1ec894a2aa1
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-12-08 14:29:03 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,1 +1,75 @@
-(({runtime:r,contextMenus:m,management:g,action:o,commands:c,scripting:s,downloads:d,tabs:t,system:y})=>(r.onInstalled.addListener(()=>m.create({id:"",title:"Snap video frame",contexts:["page","video"]})),r=(a,b)=>(b??=a).url[0]=="c"||s.executeScript({target:(a=a?.frameId)?{tabId:b.id,frameIds:[a]}:{tabId:b.id},world:"MAIN",func:async()=>{let v=document.getElementsByTagName("video"),i=v.length;if(i){let k=Array(i);while(k[--i]=v[i].offsetWidth,i);if((v=v[k.indexOf(Math.max(...k))]).readyState){v.pause();try{return[k=v.currentTime,((i=new OffscreenCanvas(v.videoWidth,v.videoHeight)).getContext("bitmaprenderer").transferFromImageBitmap(await createImageBitmap(v)),URL.createObjectURL(await i.convertToBlob()))]}catch(e){return(e=document).fullscreenElement??(setTimeout(e.exitFullscreen.bind(e),4e3),await v.requestFullscreen({navigationUI:"hide"})),[k,v.videoWidth,v.videoHeight]}}}}},async e=>(e&&=e[0].result)&&((r=(await g.getAll()).find(v=>v.name=="file.format"))&&r.enabled?await g.setEnabled(r=r.id,!1):r=0,await d.download({filename:b.title.replace(/[|?":/<>*\\]/g,"_")+"-"+((a=e[0])>=3600?(a/3600^0)+"h-":"")+((m=a%3600/60^0)?m+"m-":"")+((m=a%60^0)?m+"s-":"")+((m=(a%60-m)*1e3)^0)+"ms.png",url:e.length<3?e[1]:(m=await t.captureVisibleTab(b.windowId,{format:"png"}),(a=(await y.display.getInfo())[0].bounds).width*a.dpiX==(b=e[1])*96&&a.height*a.dpiY==e[2]*96?m:await new Promise(async p=>((a=new OffscreenCanvas(b,e[2])).getContext("bitmaprenderer").transferFromImageBitmap(await createImageBitmap(await(await fetch(m)).blob(),{resizeWidth:b,resizeHeight:e[2],resizeQuality:"high"})),(e=new FileReader).onload=()=>p(e.result),e.readAsDataURL(await a.convertToBlob()))))}),r&&g.setEnabled(r,!0))),m.onClicked.addListener(r),o.onClicked.addListener(r),c.onCommand.addListener(r)))(chrome)
+{
+let f =(a, b)=>
+  (b ??= a).url[0]=="c" || chrome.scripting.executeScript({
+    target: (a = a?.frameId) ? {tabId: b.id, frameIds: [a]} : {tabId: b.id},
+    world: "MAIN",
+    func: async ()=> {
+      let v = document.getElementsByTagName("video"),
+          i = v.length
+      if (i) {
+        let n = 0, w = 0, t = 0
+        while (w < (t = v[--i].offsetWidth) && (w = t, n = i), i);
+        if ((v = v[n]).readyState) {
+          v.pause()
+          try {
+            return [
+              v.currentTime,
+              ((n = new OffscreenCanvas(v.videoWidth, v.videoHeight))
+                .getContext("bitmaprenderer")
+                  .transferFromImageBitmap(await createImageBitmap(v)),
+              URL.createObjectURL(await n.convertToBlob()))
+            ]
+          } catch (e) {
+            document.fullscreenElement ?? setTimeout(()=> document.exitFullscreen(), 4000)
+            await v.requestFullscreen({navigationUI: "hide"})
+            return [
+              v.currentTime,
+              v.videoWidth,
+              v.videoHeight
+            ]
+          }
+        }
+      }
+    }
+  }, async e=> {
+    if (e &&= e[0].result) {
+      let n, x = await chrome.management.getAll()
+      (x = x.find(v=> v.name == "file.format")) && x.enabled ?
+        await chrome.management.setEnabled(x = x.id, !1) : x = 0
+      await chrome.downloads.download({
+        filename: b.title.replace(/[|?":/<>*\\]/g,"_") + "-" +
+          ((a = e[0]) >= 3600 ? (a / 3600 ^0) + "h-": "") +
+          ((n = a % 3600 / 60 ^0) ? n + "m-": "") +
+          ((n = a % 60 ^ 0) ? n + "s-" : "") +
+          ((n = (a % 60 - n)* 1000) ^0) + "ms.png",
+        url: e.length < 3 ? e[1] : (
+          n = await chrome.tabs.captureVisibleTab(b.windowId, {format: "png"}),
+          (a = (await chrome.system.display.getInfo())[0].bounds).width * a.dpiX == (b = e[1]) * 96 &&
+          a.height * a.dpiY == e[2] * 96 ? n:
+          await new Promise(async p=> (
+            (a = new OffscreenCanvas(b, e[2]))
+              .getContext("bitmaprenderer")
+                .transferFromImageBitmap(await createImageBitmap(
+                  await(await fetch(n)).blob(),
+                  {resizeWidth: b, resizeHeight: e[2], resizeQuality:"high"}
+                )),
+            (e = new FileReader).onload =()=> p(e.result),
+            e.readAsDataURL(await a.convertToBlob())
+          ))
+        )
+      })
+      x && chrome.management.setEnabled(x, !0)
+    }
+  })
+
+chrome.action.onClicked.addListener(f)
+chrome.contextMenus.onClicked.addListener(f),
+chrome.commands.onCommand.addListener(f)
+}
+chrome.runtime.onInstalled.addListener(()=>
+  chrome.contextMenus.create({
+    id: "",
+    title: "Snap video frame",
+    contexts: ["page","video"]
+  })
+)
