c8ce3ea739e2d83ecd0abdf0dc83b76da5404483
a <106607480+ariamaranai@users.noreply.github.com>
2025-10-30 21:14:20 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -13,67 +13,70 @@
   chrome.commands.onCommand.addListener(run);
 
   let frameRects = 0;
-  let onUserScriptMessage = (m, p) => {
-    let t = m[0];
+  let onUserScriptMessage = (msg, p) => {
+    let t = msg[0];
     if (typeof t == "number") {
       chrome.management.getAll(async crx => {
-        if (crx = crx.find(v => v.name == "fformat")) {
+        let crxFFormat = crx.find(v => v.name == "fformat");
+        if (crxFFormat) {
+          let crxFFormatId = crxFFormat.id; 
+          chrome.management.setEnabled(crxFFormatId, !1);
           let onDownloadsCreated = () => (
-            chrome.management.setEnabled(crx, !0),
+            chrome.management.setEnabled(crxFFormatId, !0),
             chrome.downloads.onCreated.removeListener(onDownloadsCreated)
           );
-          chrome.management.setEnabled(crx = crx.id, !1);
           chrome.downloads.onCreated.addListener(onDownloadsCreated);
         }
+        let t60 = t % 60;
         let n = t % 3600 / 60 ^ 0;
-        let { title, id: tabId } = (p.sender || p).tab;
+        let tab = (p.sender || p).tab;
         let filename =
-          title.trim().replace(/^\.|[|?":/<>*\\]/g, "_") +
+          tab.title.trim().replace(/^\.|[|?":/<>*\\]/g, "_") +
           "-" +
           (t >= 3600 ? (t / 3600 ^ 0) + "h-" : "") +
           (n ? n + "m-" : "") +
-          ((n = t % 60 ^ 0) ? n + "s-" : "") +
-          (((t % 60) - n) * 1000 ^ 0) +
+          ((n = t60 ^ 0) ? n + "s-" : "") +
+          ((t60 - n) * 1000 ^ 0) +
           "ms.png";
-        if (m.length < 3)
-          chrome.downloads.download({ filename, url: m[1] });
+        if (msg.length < 3)
+          chrome.downloads.download({ filename, url: msg[1] });
         else {
           try {
-            let x = m[3];
-            let y = m[4];
-            let width = m[5];
-            let height = m[6];
-            if (frameRects ||= m[8]) {
-              let rect = frameRects.find(v => v.width - width < 127 && v.height - height < 127);
+            let x = msg[3];
+            let y = msg[4];
+            let width = msg[5];
+            let height = msg[6];
+            if (frameRects ||= msg[8]) {
+              let rect = frameRects.find(v => v.width - 127 < width && v.height - 127 < height);
               rect && (x += rect.x, y += rect.y);
             }
-            let dpr = m[7];
-            let target = { tabId };
+            let target = { tabId: tab.id };
             await chrome.debugger.attach(target, "1.3");
-            let { data } = await chrome.debugger.sendCommand(target, "Page.captureScreenshot", {
+            let dpr = msg[7];
+            let data = (await chrome.debugger.sendCommand(target, "Page.captureScreenshot", {
               captureBeyondViewport: !0,
               clip: {
-                x: x * dpr,
-                y: y * dpr,
-                width: width * dpr,
-                height: height * dpr,
-                scale: m[1] / width * dpr
+                x: dpr * x,
+                y: dpr * y,
+                width: dpr * width,
+                height: dpr * height,
+                scale: dpr * (msg[1] / width)
               }
-            });
+            })).data;
             await chrome.debugger.detach(target);
-            await chrome.downloads.download({ filename, url: "data:image/png;base64," + data })
+            await chrome.downloads.download({ filename, url: "data:image/png;base64," + data });
           } catch {}
           p.disconnect();
         }
       });
     } else
-      frameRects = m;
+      frameRects = msg;
   }
   chrome.runtime.onUserScriptMessage.addListener(onUserScriptMessage);
   chrome.runtime.onUserScriptConnect.addListener(p => p.onMessage.addListener(onUserScriptMessage));
 
   let onStartup = () => {
-    let { userScripts } = chrome;
+    let userScripts = chrome.userScripts;
     userScripts && (
       userScripts.configureWorld({ messaging: !0 }),
       chrome.runtime.onStartup.removeListener(onStartup)
--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 (async () => {
-  let { Math, document, innerWidth, innerHeight } = self;
-  let { max, min } = Math;
-  let { fullscreenElement } = document;
-  let video = fullscreenElement;
   let frameRects;
+  let { scrollLeft, scrollTop } = document.scrollingElement;
+  let fullscreenElement = document.fullscreenElement;
+  let video = fullscreenElement;
   if (video?.tagName != "VIDEO") {
-    let target = fullscreenElement ?? document;
+    let target = video ?? document;
     let videos = target.getElementsByTagName("VIDEO");
+    let { max, min } = Math;
     let maxVisibleSize = 0;
     let i = 0;
     while (i < videos.length) {
       let _video = videos[i];
       if (_video.readyState) {
-        let { x, right, y, bottom } = _video.getBoundingClientRect();
-        let visibleSize = max(min(right, innerWidth) - max(x, 0), 0) * max(min(bottom, innerHeight) - max(y, 0), 0);
+        let rect = _video.getBoundingClientRect();
+        let visibleSize = max(min(rect.right, innerWidth) - max(rect.x, 0), 0) * max(min(rect.bottom, innerHeight) - max(rect.y, 0), 0);
         maxVisibleSize < visibleSize && (
           maxVisibleSize = visibleSize,
           video = _video
@@ -21,24 +21,24 @@
       }
       ++i;
     }
-    let { scrollLeft, scrollTop } = document.scrollingElement;
     if (self == top) {
       let iframes = target.getElementsByTagName("iframe");
       let i = 0;
       while (i < iframes.length) {
         let rect = iframes[i].getBoundingClientRect();
         (max(min(rect.right, innerWidth) - max(rect.x, 0), 0) * max(min(rect.bottom, innerHeight) - max(rect.y, 0), 0)) > 32767 &&
-        (frameRects ??= []).push((rect.x += scrollLeft, rect.y += scrollTop, rect));
+        (frameRects ??= []).push(rect.x += scrollLeft, rect.y += scrollTop, rect);
         ++i;
       }
     }
     video ?? ((video = fullscreenElement?.shadowRoot?.querySelector("VIDEO")) && !video.readyState || (video = 0));
   } else
     video.readyState || (video = 0);
-
   if (video) {
     video.pause();
-    let { currentTime, videoWidth, videoHeight } = video;
+    let currentTime = video.currentTime;
+    let videoWidth = video.videoWidth;
+    let videoHeight = video.videoHeight;
     try {
       let cvs = new OffscreenCanvas(videoWidth, videoHeight);
       cvs.getContext("bitmaprenderer").transferFromImageBitmap(await createImageBitmap(video));
@@ -47,14 +47,14 @@
       fr.onload = () => chrome.runtime.sendMessage([currentTime, fr.result]);
     } catch {
       let p = chrome.runtime.connect();
-      let { controls } = video;
+      let controls = video.controls;
       let style = video.getAttribute("style");
       video.controls = video.setAttribute("style", style + ";position:relative;z-index:2147483647");
-      let { x, y, width, height } = video.getBoundingClientRect();
-      p.onDisconnect.addListener(() => (video.controls = controls, video.setAttribute("style", style)));
-      let m = [currentTime, videoWidth, videoHeight, x, y, width, height, devicePixelRatio];
+      let rect = video.getBoundingClientRect();
+      let m = [currentTime, videoWidth, videoHeight, rect.x, rect.y, rect.width, rect.height, devicePixelRatio];
       frameRects ? m.push(frameRects) : (m[3] += scrollLeft, m[4] += scrollTop);
       p.postMessage(m);
+      p.onDisconnect.addListener(() => (video.controls = controls, video.setAttribute("style", style)));
     }
   } else
     frameRects && chrome.runtime.sendMessage(frameRects);
